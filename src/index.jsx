5f7613837ec2ff0275af637f71d952d737931514
prasanna1211 <prasanna@codebrahma.com>
2017-11-11 10:10:48 +0530

    Added redux form compatiblity

--- src/index.jsx
+++ src/index.jsx
@@ -6,6 +6,18 @@ require("../styles/application.scss");
 // Render the top-level React component
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createStore, applyMiddleware, combineReducers } from 'redux';
+import { Provider } from 'react-redux';
+import { reducer as formReducer } from 'redux-form';
+
 import App from './App.jsx';
 
-ReactDOM.render(<App />, document.getElementById('react-root'));
+const rootReducer = combineReducers({
+  form: formReducer,
+})
+
+const store = createStore(rootReducer);
+
+ReactDOM.render(<Provider store={store}>
+    <App />
+  </Provider>, document.getElementById('react-root'));
