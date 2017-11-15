5553d85d993578c7abe838c8f42144ef9e755b5c
prasanna1211 <prasanna@codebrahma.com>
2017-11-15 10:05:05 +0530

    added radio button group

--- src/formComponents/semanticUI/index.js
+++ src/formComponents/semanticUI/index.js
@@ -2,11 +2,16 @@ import React from 'react';
 import TextField from './TextField.js';
 import CheckBox from './CheckBox.js';
 import Select from './Select.js'; 
+import RadioButtonGroup from './RadioButtonGroup.js';
 
 const options = [{
   value: 'optionA',
   key: 'optionA',
   text: 'optionA',
+}, {
+  value: 'optionB',
+  key: 'optionB',
+  text: 'optionB',
 }];
 
 const Form = () => (
@@ -21,6 +26,10 @@ const Form = () => (
     />
     <Select
       label="Select"
+      options={options}
+    />
+    <RadioButtonGroup
+      label="radio"
       options={options}
     />
   </div>
