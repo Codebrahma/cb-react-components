cce4ae8dd43e7b7869580f7bfe88afa7ba3f6284
Dass Mk <kalidasm610@gmail.com>
2017-11-16 18:42:16 +0530

    Sample Mui Formsy Form setup

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,12 @@ import React, {Component} from 'react';
 import { Form } from 'formsy-react';
 
 import TextField from './formComponents/MaterialUI/Formsy/TextField.js';
-import Checkbox from './formComponents/MaterialUI/Formsy/CheckBox.js';
-import RadioButtonGroup from './formComponents/MaterialUI/Formsy/RadioButtonGroup.js';
+import CheckBox from './formComponents/MaterialUI/Formsy/CheckBox.js';
+import RadioGroup from './formComponents/MaterialUI/Formsy/RadioGroup.js';
+
+import MuiTextField from './formComponents/MaterialUI/TextField';
+import MuiRadio from './formComponents/MaterialUI/Radio';
+import MuiRadioGroup from './formComponents/MaterialUI/RadioGroup';
 
 class App extends Component {
   constructor(props) {
@@ -11,35 +15,61 @@ class App extends Component {
 
   }
 
+  onSubmit = (data) => {
+    console.log(data);
+  }
+
   render() {
     return (
-      <Form
+      <div>
+        <Form
         onSubmit={this.onSubmit}
-      >
-        <TextField 
-          name="emailAddress"
-          validations="isEmail" 
-          validationError="This is not a valid email"
-          requiredError='Field Required Custom Message'
-          required
-          label="Email Address"
-        />
-        <Checkbox
-          name="checked"
-          label="Cbox Label"
-        />
-        <RadioButtonGroup
-          name="optionSelected"
-          radioOptions={[
-            { name: 'React', value: 'react', disabled: false },
-            { name: 'Redux', value: 'redux', disabled: false },
-            { name: 'Flux', value: 'flux', disabled: true },
-          ]}
-        />
-        <button>
-          Submit
-        </button>
-      </Form>
+        >
+          <div style={{paddingBottom: '40px'}}>
+            <TextField
+              name="emailAddress"
+              validations="isEmail"
+              validationError="This is not a valid email"
+              label="Email Address"
+              helperText="Helper Text: Field to enter Email"
+              required
+              errorMessage='asf'
+              helperText='Help'
+              disabled={false}
+            />
+          </div>
+
+          <div style={{ paddingBottom: '40px' }}>
+            <RadioGroup
+              label="RadioGroup"
+              name="optionSelected"
+              error={false}
+              errorMessage='asf'
+              helperText='Help'
+              required
+              radioOptions={[
+                { label: 'React', value: 'react', disabled: false },
+                { label: 'Redux', value: 'redux', disabled: false },
+                { label: 'Flux', value: 'flux', disabled: false },
+              ]}
+            />
+          </div>
+
+          <div style={{ paddingBottom: '40px' }}>
+            <CheckBox
+              name="Checkbox"
+              label="CBox Label"
+              value={false}
+            />
+          </div>
+
+          <div>
+            <button>
+              Submit
+            </button>
+          </div>
+        </Form>
+      </div>
     )
   }
 }
