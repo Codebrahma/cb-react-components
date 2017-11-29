f800889d63e7891571ff24a0d8294a108964d463
prasanna1211 <prasanna@codebrahma.com>
2017-11-29 11:52:05 +0530

    basic restructuring

--- src/index.jsx
+++ src/index.jsx
@@ -12,24 +12,17 @@ import { Provider } from 'react-redux';
 import logger from 'redux-logger';
 import { reducer as formReducer } from 'redux-form';
 
+// Required for React MDL
 import 'react-mdl/extra/material.css';
 import 'react-mdl/extra/material.js';
 
+// Required for Material UI
 import { MuiThemeProvider, createMuiTheme } from 'material-ui/styles';
+
+// Required for DatePicker
 import 'react-dates/initialize';
 
-import App from './App.jsx';
-
-import {
-  isRequired,
-  isNumber,
-  isEmail,
-  isAlphaNumeric, 
-  isMobileNumber, 
-  maxLength,
-  minLength,
-  minValue,
-} from './FormBindings/Validations/index.jsx';
+import App from './Demo';
 
 const rootReducer = combineReducers({
   form: formReducer,
@@ -39,68 +32,9 @@ const theme = createMuiTheme();
 
 const store = createStore(rootReducer, {}, applyMiddleware(logger));
 
-const onSubmit = (values) => {
-  console.log('Form Values', values);
-};
-
-const validate = values => {
-  const errors = {}
-  if (!values.name) {
-    errors.name = 'Required Name'
-  } else if (values.name.length > 15) {
-    errors.name = 'Must be 15 characters or less'
-  }
-  return errors
-};
-
-{/*
-  Structure of Validation Object for Field Level Validation
-
-  {
-    `elementName`: {
-      validations: [validationFunctions],
-      warnings: [warningFunctions],
-    },
-    ...,
-    ,..
-  }
-
-*/}
-
-const fieldsValidationConfig = {
-  'name': {
-    validations: [(v) => ((`VName ${v}`))],
-    warnings: [(v) =>((`VName ${v}`))]
-  },
-  'gender': {
-    validations: [(v) =>((`VName ${v}`))],
-    warnings: [(v) => ((`WName ${v}`))],
-  },
-  'dob': {
-    validations: [(v) =>((`VName ${v}`))],
-    warnings: [(v) => ((`WName ${v}`))],
-  },
-  'Subscription': {
-    validations: [(v) =>((`VName ${v}`))],
-    warnings: [(v) => ((`WName ${v}`))],
-  },
-  'country': {
-    validations: [(v) =>((`VName ${v}`))],
-    warnings: [(v) => ((`WName ${v}`))],
-  },
-  'isRegistered': {
-    validations: [(v) =>((`VName ${v}`))],
-    warnings: [(v) => ((`WName ${v}`))],
-  },
-};
-
 ReactDOM.render(<Provider store={store}>
     <MuiThemeProvider theme={theme}>
       <App
-        onSubmit={onSubmit}
-        name='DemoForm'
-        // formValidator={validate}
-        // fieldsValidationConfig={fieldsValidationConfig}
       />
     </MuiThemeProvider>
   </Provider>, document.getElementById('react-root'));
