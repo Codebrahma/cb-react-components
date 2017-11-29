798410b3c36ea0f15a3e76bde14071deac5674e6
Dass Mk <kalidasm610@gmail.com>
2017-11-29 12:25:13 +0530

    Formsy Index Structure defined

---
+++ src/FormBindings/FormsyForm/Components/index.js
@@ -0,0 +1,17 @@
+import CheckBoxFormBinder from './CheckBox.js';
+import CheckBoxGroupFormBinder from './CheckBoxGroup.js';
+import DatePickerFormBinder from './DatePicker.js';
+import RadioGroupFormBinder from './RadioGroup.js';
+import SelectFormBinder from './Select.js';
+import TextFieldFormBinder from './TextField.js';
+import ToggleFormBinder from './Toggle.js';
+import SliderFormBinder from './Slider.js';
+
+export const CheckBox = CheckBoxFormBinder;
+export const CheckBoxGroup = CheckBoxGroupFormBinder;
+export const DatePicker = DatePickerFormBinder;
+export const RadioButtonGroup = RadioGroupFormBinder;
+export const Select = SelectFormBinder;
+export const TextField = TextFieldFormBinder;
+export const Toggle = ToggleFormBinder;
+export const Slider = SliderFormBinder;
--- src/FormComponents/MaterialUI/Radio.js
+++ src/FormComponents/MaterialUI/Radio.js
@@ -14,6 +14,7 @@ const MuiRadio = (muiRadioProps) => {
 
   return (
     <FormControlLabel
+      label={formControlLabelProps['text']}
       {...formControlLabelProps}
       control={(
         <Radio
