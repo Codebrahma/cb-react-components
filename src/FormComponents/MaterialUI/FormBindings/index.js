3c1c14bb9d11b6b39554b8d95716c26a7d7bc2be
prasanna1211 <prasanna@codebrahma.com>
2017-12-12 08:21:17 +0530

    restructured app

--- src/FormComponents/MaterialUI/FormBindings/index.js
+++ src/FormComponents/MaterialUI/FormBindings/index.js
@@ -1,3 +1,4 @@
+import ButtonFormBinder from './Button.js';
 import CheckBoxFormBinder from './CheckBox.js';
 import DatePickerFormBinder from './DatePicker.js';
 import RadioButtonGroupFormBinder from './RadioButtonGroup.js';
@@ -5,6 +6,7 @@ import SelectFormBinder from './Select.js';
 import TextFieldFormBinder from './TextField.js';
 import ToggleFormBinder from './Toggle.js';
 
+export const Button = ButtonFormBinder;
 export const CheckBox = CheckBoxFormBinder;
 export const DatePicker = DatePickerFormBinder;
 export const RadioButtonGroup = RadioButtonGroupFormBinder;
