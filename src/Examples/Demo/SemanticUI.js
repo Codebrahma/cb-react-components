70503153a3b072d37ee4b738c4db12c5af2af5bf
prasanna1211 <prasanna@codebrahma.com>
2017-12-17 11:03:31 +0530

    added menu

---
+++ src/Examples/Demo/SemanticUI.js
@@ -0,0 +1,84 @@
+import React, {Component} from 'react';
+import { connect } from 'react-redux';
+import { reduxForm } from 'redux-form';
+import {
+  TextField,
+  RadioButtonGroup,
+  Toggle,
+  CheckBox,
+  Select,
+} from '../../FormComponents/SemanticUI/FormBindings/ReduxForm';
+
+import Form from '../../FormComponents/SemanticUI/FormBindings/ReduxForm/Form';
+import { 
+  styles
+} from './styles';
+import {
+  genderOptions,
+  countryOptions,
+  tShirtOptions,
+} from './mock';
+
+import {
+  validationConfig
+} from './validation';
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <div style={styles.formWrapperStyle}> 
+      <Form
+        {...this.props}
+        fieldsValidationConfig={validationConfig}
+      > 
+        <TextField
+          name="name"
+          label="Name"
+          {...styles}
+        />
+        <TextField
+          name="email"
+          label="Email Address"
+          {...styles}
+        />
+        <RadioButtonGroup 
+          name="tshirtSize"
+          label="T-shirt Size"
+          options={tShirtOptions}
+          {...styles}            
+        />
+        <Toggle 
+          name="surprise"
+          label="Special surprise?"
+          {...styles}
+        />
+        <Select
+          name="country"
+          label="Country"
+          options={countryOptions}
+          {...styles}
+        />
+        <CheckBox 
+          name="acceptTerms"
+          label="Accept Terms ?"
+          {...styles}
+        />
+        <div>
+          <button>
+            Submit
+          </button>
+        </div>
+      </Form>
+      </div>
+    )
+  }
+}
+
+export default reduxForm({
+  form: 'demo',
+  fieldsValidationConfig: validationConfig,
+})(App);
--- src/Examples/index.js
+++ src/Examples/index.js
@@ -1,16 +1,59 @@
-import React from 'react';
-import Playground from './Demo';
+import React, { Component } from 'react'
+import { Menu } from 'semantic-ui-react'
+import MaterialUI from './Demo/MaterialUI';
+import SemanticUI from './Demo/SemanticUI';
 
-const Demo = () => {
+const Demo = ({ activeItem }) => {
   const onSubmit = (values) => {
     console.log('submit is ', values);
   }
-  return (
-    <Playground
+  return activeItem === 'material' ? (
+    <MaterialUI
+      name='DemoForm'
+      onSubmit={onSubmit}
+    />
+  ) : (
+    <SemanticUI
       name='DemoForm'
       onSubmit={onSubmit}
     />
   )
 }
 
-export default Demo;
+export default class MenuExampleBasic extends Component {
+  state = { activeItem: 'material' }
+
+  handleItemClick = (e, { name }) => this.setState({ activeItem: name })
+
+  render() {
+    const { activeItem } = this.state
+
+    return (
+      <div>
+      <Menu>
+        <Menu.Item
+          name='material'
+          active={activeItem === 'material'}
+          onClick={this.handleItemClick}
+        >
+          Material UI
+        </Menu.Item>
+
+        <Menu.Item
+          name='semantic'
+          active={activeItem === 'semantic'}
+          onClick={this.handleItemClick}
+        >
+          Semantic UI
+        </Menu.Item>
+
+        
+      </Menu>
+      <Demo
+        activeItem={activeItem}
+      />
+    </div>
+      
+    )
+  }
+}
