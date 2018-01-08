3c6ba6bcc7254e5a697ebacb23c9716169cca9c8
ankushCb <ankush@codebrahma.com>
2018-01-09 00:55:45 +0530

    Add password input field for material-ui-next and semantic-ui-next

--- src/Root/MaterialUI_ReduxForm/Components/index.js
+++ src/Root/MaterialUI_ReduxForm/Components/index.js
@@ -1,9 +1,11 @@
 import { InputText as _InputText } from './InputText.js';
 import { InputSelect as _InputSelect } from './InputSelect.js';
 import { MultiSelect as _MultiSelect} from './MultiSelect.js';
+import { InputPassword as _InputPassword } from './InputPassword.js';
 
 export {
   _InputText as InputText,
   _InputSelect as InputSelect,
-  _MultiSelect as MultiSelect
+  _MultiSelect as MultiSelect,
+  _InputPassword as InputPassword
 }
---
+++ src/Root/SemanticUI_ReduxForm/Components/InputPassword.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import { FormStructureHOC } from '../Common/FormStructureHOC.js';
+import { Input as PasswordField } from 'semantic-ui-react';
+import { Field } from 'redux-form';
+
+/* Simple Input imported from Semantic UI for password field*/
+const _InputPasswordUI = (props) => (
+  <PasswordField
+    {...props}
+    fluid
+  />
+);
+
+/* A Higher order component which adds a layout for form */
+/* Look at the corresponding HOC to know more */
+const InputPasswordUI = FormStructureHOC(_InputPasswordUI);
+
+/* If you are passing removeFormLayoutProp then you get the stand alone component binded with the form */
+/* If you are working on custom form element wrapper either write a new HOC */
+export const InputPassword = ({ name, ...props }) => (
+  <Field
+    name={name}
+    component={InputPasswordUI}
+    type="password"
+    {...props}
+  />
+);
