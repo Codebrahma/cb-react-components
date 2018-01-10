6c1eff3e2d0c8a8704d5e09ae23db8cb23d3eeb2
ankushCb <ankush@codebrahma.com>
2018-01-10 16:04:23 +0530

    Password -ui for the bootstrap

---
+++ src/Root/BootstrapUI_ReduxForm/Components/InputPassword.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import { FormStructureHOC } from '../Common/FormStructureHOC.js';
+import { Field } from 'redux-form';
+
+/* Simple input using bootstrap class */
+const _InputPasswordUI = (props) => {
+  return (
+    <input
+      type="password"
+      className="form-control"
+      {...props}
+      />
+  );
+}
+
+/* A Higher order component which adds a layout for form */
+/* Look at the corresponding HOC to know more */
+const InputPasswordUI = FormStructureHOC(_InputPasswordUI);
+
+/* If you are passing removeFormLayoutProp then you get the stand alone component binded with the form */
+/* If you are working on custom form element wrapper either write a new HOC */
+export const InputPassword = ({ name, ...props }) => (
+  <Field
+    name={name}
+    component={InputPasswordUI}
+    {...props}
+  />
+);
--- src/Root/BootstrapUI_ReduxForm/Components/index.js
+++ src/Root/BootstrapUI_ReduxForm/Components/index.js
@@ -3,11 +3,13 @@ import { InputCheckbox } from './InputCheckbox.js';
 import { InputGroupCheckbox } from './InputGroupCheckbox.js';
 import { CustomButton } from './CustomButton.js';
 import { InputRadioButtons } from './InputRadioButtons.js';
+import { InputPassword } from './InputPassword.js';
 
 export {
   InputText,
   InputCheckbox,
   InputGroupCheckbox,
   CustomButton,
-  InputRadioButtons
+  InputRadioButtons,
+  InputPassword
 }
