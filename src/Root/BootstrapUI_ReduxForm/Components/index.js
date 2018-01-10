784840a470c334c0d348721fbfffaa2b4ace7455
ankushCb <ankush@codebrahma.com>
2018-01-10 16:36:16 +0530

    Checkbox and checkbox group -UI for bootstrap.

--- src/Root/BootstrapUI_ReduxForm/Components/index.js
+++ src/Root/BootstrapUI_ReduxForm/Components/index.js
@@ -1,5 +1,9 @@
 import { InputText } from './InputText.js';
+import { InputCheckbox } from './InputCheckbox.js';
+import { InputGroupCheckbox } from './InputGroupCheckbox.js';
 
 export {
-  InputText
+  InputText,
+  InputCheckbox,
+  InputGroupCheckbox,
 }
