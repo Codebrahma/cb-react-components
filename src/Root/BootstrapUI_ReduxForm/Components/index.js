21d838dd91ca23cca00eda4457e38dfad1ba70a1
ankushCb <ankush@codebrahma.com>
2018-01-10 17:41:47 +0530

    Radio buttons with bootstrap.

--- src/Root/BootstrapUI_ReduxForm/Components/index.js
+++ src/Root/BootstrapUI_ReduxForm/Components/index.js
@@ -2,10 +2,12 @@ import { InputText } from './InputText.js';
 import { InputCheckbox } from './InputCheckbox.js';
 import { InputGroupCheckbox } from './InputGroupCheckbox.js';
 import { CustomButton } from './CustomButton.js';
+import { InputRadioButtons } from './InputRadioButtons.js';
 
 export {
   InputText,
   InputCheckbox,
   InputGroupCheckbox,
-  CustomButton
+  CustomButton,
+  InputRadioButtons
 }
