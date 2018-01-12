4aaabd27601db32edd64fcd318cbb7b06f9eb309
prasanna1211 <prasanna@codebrahma.com>
2018-01-12 08:59:20 +0530

    Added form code

--- src/Examples/index.js
+++ src/Examples/index.js
@@ -1,11 +1,23 @@
 import React from 'react';
-import PlayGroundForm from './PlayGroundForm.jsx';
+import DemoApp from './Demo1/index.jsx';
 
 class App extends React.Component {
+  state = {}
+  componentDidCatch(err, errorInfo) {
+    console.log('error found ', err);
+  }
+
   render() {
+    if (this.state.err) {
+      return (
+        <div>
+          {this.state.err}
+        </div>
+      )
+    }
     return (
       <div>
-        <PlayGroundForm
+        <DemoApp
           onSubmit={() => { console.log('hello') }}
         />
       </div>
